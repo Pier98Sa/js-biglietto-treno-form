33b0d292989cb39a36da97b4588509143363bf2f
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-01-24 20:20:24 +0100

    aggiunte tutte le funzioni

--- main.js
+++ main.js
@@ -11,32 +11,43 @@ let buttonAnnulla = document.getElementById("annulla");
 
 buttonGenera.addEventListener('click',    
     function() {
+        
         //variabili immesse dall'utente
         let name = document.getElementById("name").value;
+        document.getElementById('name-ticket').innerHTML = name;
                 
         let km = document.getElementById("km").value;
 
         let age = document.getElementById("age").value;
         
+        //variabile inizializzata fuori dal ciclo if
         let prezzoBiglietto;
 
+        //funzioni per la creazione di numeri randomici
+        document.getElementById('train-car').innerHTML = (Math.floor(Math.random() * 10));
+        document.getElementById('cp-code').innerHTML = (Math.floor(Math.random() * 9999)+ 90000);
+
+        
         if (age == "under-18") {
             // calcolo costo del biglietto
             prezzoBiglietto = km * prezzoKm * sconto20;
-            alert("il prezzo del biglietto è :" + prezzoBiglietto );
+            document.getElementById('ticket-price').innerHTML = (prezzoBiglietto.toFixed(2)) + "\u20AC";
         } else if (age == "over-65"){
             // calcolo costo del biglietto
             prezzoBiglietto = km * prezzoKm * sconto40;
-            alert("il prezzo del biglietto è :" + prezzoBiglietto );
+            document.getElementById('ticket-price').innerHTML = (prezzoBiglietto.toFixed(2)) + "\u20AC";
         }else if (age == ""){
             alert("Fascia d'età non valida")
         }else {
             // calcolo costo del biglietto
             prezzoBiglietto = km * prezzoKm ;
-            
-           alert("il prezzo del biglietto è :" + prezzoBiglietto );
+           document.getElementById('ticket-price').innerHTML = (prezzoBiglietto.toFixed(2)) + "\u20AC";
 
         }
+
+        //aggiunta della classe active per mostrare il risultato
+        const htmlGeneratedTicket = document.querySelector('.generated-ticket');
+        htmlGeneratedTicket.classList.add('active');
     }
 );
 
@@ -44,5 +55,8 @@ buttonGenera.addEventListener('click',
 buttonAnnulla.addEventListener('click',    
     function() {
         document.getElementById("myForm").reset();
+        //rimozione della classe active per resettare la pagina
+        const htmlGeneratedTicket = document.querySelector('.generated-ticket');
+        htmlGeneratedTicket.classList.remove('active');
     }
 );
