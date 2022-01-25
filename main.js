280ee98dc48e2f5e8466eebcda368a09bd0ec1bd
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-01-25 10:43:03 +0100

    modificata la function in Js

--- main.js
+++ main.js
@@ -23,35 +23,36 @@ buttonGenera.addEventListener('click',
         
         //variabile inizializzata fuori dal ciclo if
         let prezzoBiglietto;
-
+        let offerta;
         //funzioni per la creazione di numeri randomici
+        //codice della carrozza numero tra 1 e 9
         document.getElementById('train-car').innerHTML = (Math.floor(Math.random() * 9)+1);
+        //codice CP numero tra 90000 e 99999
         document.getElementById('cp-code').innerHTML = (Math.floor(Math.random() * 9999)+ 90000);
 
         
         if (age == "under-18") {
             // calcolo costo del biglietto
             prezzoBiglietto = km * prezzoKm * sconto20;
-            //stampa del prezzo e dell'offerta
-            document.getElementById('ticket-price').innerHTML = (prezzoBiglietto.toFixed(2)) + "\u20AC";
-            document.getElementById('ticket-type').innerHTML = 'Biglietto scontato -20%';
+            //valore della variabile offerta
+            offerta = "Biglietto scontato -20%";  
         } else if (age == "over-65"){
             // calcolo costo del biglietto
             prezzoBiglietto = km * prezzoKm * sconto40;
-            //stampa del prezzo e dell'offerta
-            document.getElementById('ticket-price').innerHTML = (prezzoBiglietto.toFixed(2)) + "\u20AC"
-            document.getElementById('ticket-type').innerHTML = 'Biglietto scontato -40%';
+            //valore della variabile offerta
+            offerta = "Biglietto scontato -40%";  
         }else if (age == ""){
             alert("Fascia d'età non valida")
+
         }else {
             // calcolo costo del biglietto
             prezzoBiglietto = km * prezzoKm ;
-            //stampa del prezzo e dell'offerta
-           document.getElementById('ticket-price').innerHTML = (prezzoBiglietto.toFixed(2)) + "\u20AC";
-           document.getElementById('ticket-type').innerHTML = 'Biglietto standard';
-
+            //valore della variabile offerta
+            offerta = "Biglietto standard";
         }
 
+        document.getElementById('ticket-price').innerHTML = (prezzoBiglietto.toFixed(2)) + "\u20AC";
+        document.getElementById('ticket-type').innerHTML = offerta;
         //aggiunta della classe active per mostrare il risultato
         const htmlGeneratedTicket = document.querySelector('.generated-ticket');
         htmlGeneratedTicket.classList.add('active');
